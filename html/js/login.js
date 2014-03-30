6202349b76b3b46044b96d5c406ecd7672a596e2
Francisco A. Granado Santos <francisco.granado1@upr.edu>
2014-03-30 10:17:40 -0400

    post and get successful

--- html/js/login.js
+++ html/js/login.js
@@ -2,5 +2,28 @@
  * @author Ch32k0
  */
 $(document).ready({
-	
+	$("#login_form").submit(function(){
+		$.ajax({
+	      url: 'http://collegelife.herokuapp.com/userlist',
+	      type: 'get',
+	      dataType: 'json',
+	      success: function (data) {
+	      	 var tableContent = '';
+			     // For each item in our JSON
+			    $.each(data, function(){
+			      if($("#student_number").val() == this._id){
+			      	window.location('platform.html');
+			      }else{
+			      	window.location('register.html');
+			      }
+			    });
+			
+			    // Inject the whole content string into our existing HTML table
+			    $('#overview_table').append(tableContent);
+	      },
+	      error: function (XMLHttpRequest, textStatus, errorThrown) {
+	        console.log('error', errorThrown);
+	      }
+	    });
+	});
 });
---
+++ html/js/register.js
@@ -0,0 +1,66 @@
+/**
+ * @author Ch32k0
+ */
+$(document).ready(function(){
+	var number;
+	var name;
+	var email;
+	var password;
+	var re_password;
+	$("#student_number").off();
+	$("#name").off();
+	$("#email").off();
+	$("#password").off();
+	$("#password_retype").off();
+	
+	$("#student_number").on("change",function(){
+		number = this.value;
+		console.log(this.value);
+	});
+	$("#name").on("change",function(){
+		name = this.value;
+	});
+	$("#student_number").on("change",function(){
+		email = this.value;
+	});
+	$("#password").on("change",function(){
+		password = this.value;
+	});
+	$("#password_retype").on("change",function(){
+		re_password = this.value;
+	});
+	
+	$("#register").click(function(){
+		if(re_password != password){
+			alert('Password must match!');
+		}else{
+			var data = {
+				"_id": number,
+				"name":name,
+				"password":password,
+				"creditos_aprovados":0,
+				"email":email,
+				"cursos":"Noob",
+				"gpa": 4		
+			};
+			console.log(data);
+			// var json = JSON.stringify(data);
+				// console.log(data);	
+			$.ajax({
+				type: "POST",
+				url: "http://collegelife.herokuapp.com/adduser",
+				data: data,
+				success: function(data){
+					console.log(data);
+				},
+				error: function(ts) { 
+					console.log(ts.responseText); 
+				}
+			});
+			window.location("platform.html");
+				
+		}
+	});
+	
+});
+
