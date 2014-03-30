e6b38e33b7b7b544529219eec8d1c2cc091addf0
Francisco A. Granado Santos <francisco.granado1@upr.edu>
2014-03-30 05:54:38 -0400

    BOOSTRAPPED FINISHED

--- html/js/CollegeLife.js
+++ html/js/CollegeLife.js
@@ -1,7 +1,24 @@
 /**
  * @author Ch32k0
  */
+var ls = window.localStorage;
 $(document).ready(function() {
-	
-});
 
+	 $.ajax({
+      url: 'http://collegelife.herokuapp.com/userlist',
+      type: 'get',
+      dataType: 'json',
+      success: function (data) {
+        console.log('success', data);
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        console.log('error', errorThrown);
+      }
+    });
+    
+    $("#Grades_panel").click(function(){
+    	 $('html, body').animate({
+        scrollTop: $("#Grades").offset().top
+    },1500);
+    });
+});
